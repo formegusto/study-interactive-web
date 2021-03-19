2ec590384b9f09f6c86cdab48e0a98b6a3f53244
formegusto <formegusto@gmail.com>
2021-03-19 23:00:13 +0900

    progress-bar

--- 15_Example/th/js/wall3d.js
+++ 15_Example/th/js/wall3d.js
@@ -1,8 +1,12 @@
 (function () {
   // 스크롤 할 때 하우스를 이동시킬 것이다.
   const houseElem = document.querySelector(".house");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  const barElem = document.querySelector(".progress-bar");
+  // 창 사이즈에 따라 바뀐다.
+  // innerHeight가 바뀌기 때문.
+  let maxScrollValue;
   window.addEventListener("scroll", function () {
+    const scrollPer = pageYOffset / maxScrollValue;
     // house의 traslateZ 값을 바꿔주면 되고,
     // 어쨌든 스크롤에 따라 움직여야 한다.
     // window.pageYOffset
@@ -18,7 +22,22 @@
     // 스크롤 하기 전에 490이었으니까 여기부터 시작하도록 설정하자.
     // 1000을 곱해주는 이유는 너무 멀기 때문
     // 1000 부분을 수정해주면, 스크롤 되는 양을 조절해줄 수 있다.
-    const zMove = (pageYOffset / maxScrollValue) * 1000 - 490;
+    const zMove = scrollPer * 1000 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
+
+  // 창 사이즈가 바뀌면 maxScrollValue를 변화시킨다.
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+  // 바뀌는 창 크기에 대응
+  window.addEventListener("resize", resizeHandler);
+
+  // 코드는 겹치지 않는 것이 좋다. 그래서 처음에 한번 실행시키고,
+  // 그 후 사이즈가 변화할 때 마다 실행되도록 한다.
+  resizeHandler();
 })();
